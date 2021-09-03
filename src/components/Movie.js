2d35b5b782a2262db8e06ebd752f7f822ece8a7b
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-03 01:07:36 -0300

    Implemented movie component (layout)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,18 +5,37 @@ import { useParams } from "react-router-dom";
 
 const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/cineflex/movies";
 
+function Day(props) {
+    const {
+        weekday,
+        date,
+        showtimes
+    } = props.day;
+
+    return (
+        <>
+            <div className="day">{weekday} - {date}</div>
+            <div className="showtimes">
+                {showtimes.map(showtime => <div className="showtime">{showtime.name}</div>)}
+            </div>
+        </>
+    );
+
+}
+
 export function Movie() {
     const params = useParams();
     const movieURL = `${URL}/${params.idMovie}/showtimes`;
-    const [movie, SetMovie] = useState()
+    const [movie, SetMovie] = useState([])
 
     useEffect(() => axios(movieURL).then((resp) => SetMovie(resp.data.days)), []);
 
     console.log(movie)
 
-
-
     return (
-        <div className="top-bar">Selecione o horário</div>
+        <>
+            <div className="top-bar">Selecione o horário</div>
+            {(movie.length > 0) ? movie.map((day) => <Day day={day} />) : "carregando..."}
+        </>
     );
 }
