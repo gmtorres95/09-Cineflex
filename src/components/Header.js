210d65599344ee035a3e64273143e02ace06eae6
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-03 10:19:12 -0300

    Implemented back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
-import "../css/header.css"
+import "../css/header.css";
+import { useHistory } from "react-router";
 
 export function Header() {
+    const history = useHistory();
     return (
-        <header>CINEFLEX</header>
+        <header>
+            <ion-icon name="arrow-back-outline" onClick={history.goBack}/>
+            CINEFLEX
+        </header>
     );
 }
