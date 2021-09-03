f08d291a2053ebc7edd8e16047597dce4116515b
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-03 00:23:01 -0300

    Partially implemented movie component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import "../css/styles.css";
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Movie } from "./Movie";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-            <Home />
-            {/* <Movie /> */}
-        </>
+			<Switch>
+				<Route path="/" exact>
+                    <Home />
+				</Route>
+				<Route path="/movie/:idMovie" exact>
+					<Movie />
+				</Route>
+			</Switch>
+		</BrowserRouter>
     );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,22 @@
-import "../css/movie.css"
+import "../css/movie.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/cineflex/movies";
 
 export function Movie() {
-    
+    const params = useParams();
+    const movieURL = `${URL}/${params.idMovie}/showtimes`;
+    const [movie, SetMovie] = useState()
+
+    useEffect(() => axios(movieURL).then((resp) => SetMovie(resp.data.days)), []);
+
+    console.log(movie)
+
+
+
+    return (
+        <div className="top-bar">Selecione o horário</div>
+    );
 }
