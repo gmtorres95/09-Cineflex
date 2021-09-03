701ed07ab94746d123513441176abfbb52e0ea42
Gabriel Torres <al2012.163.torres@gmail.com>
2021-09-03 05:33:03 -0300

    Implemented seat selection

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,3 +1,4 @@
+import "../css/session.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -13,6 +14,25 @@ function footerUpdate({movie, name, day}) {
     };
 }
 
+function Seat({seat}) {
+    let seatStyle = (seat.isAvailable) ? "available" : "unavailable";
+    const [selected, SetSelected] = useState(false);
+
+    if(selected) seatStyle = "selected";
+
+    if(Number(seat.name) % 10 === 0 && Number(seat.name) < 50) {
+        return (
+            <>
+                <li className={seatStyle} onClick={() => seat.isAvailable ? SetSelected(!selected) : ""}>{seat.name}</li>
+                <li className="dummy" />
+            </>
+        );
+    }
+    return (
+        <li className={seatStyle} onClick={() => seat.isAvailable ? SetSelected(!selected) : ""}>{seat.name}</li>
+    );
+}
+
 export function Session() {
     const params = useParams();
     const sessionURL = `${URL}/${params.idSession}/seats`;
@@ -23,11 +43,19 @@ export function Session() {
 
     if(session) footer = footerUpdate(session);
 
-    // console.log(session);
-
     return (
         <>
             <div className="top-bar">Selecione o(s) assento(s)</div>
+            <ul className="seats">
+                {session ? session.seats.map((seat) => 
+                    <Seat 
+                        seat={seat} 
+                        key={seat.id}
+                    />) : ""}
+            </ul>
+
+
+
             {session ? <Footer src={footer.src} title={footer.title} day={footer.day}/> : ""}
         </>
     );
