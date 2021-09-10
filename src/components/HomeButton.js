36001e68e0ce110986f47f783288d5cb737815b4
Matheus <matheus@respondeai.com.br>
2021-09-10 15:38:39 -0300

    preparando o review

--- src/components/HomeButton.js
+++
@@ -1,8 +0,0 @@
-import "../css/button.css";
-import { Link } from "react-router-dom";
-
-export function HomeButton() {
-    return (
-        <Link to="/" className="button">Voltar pra Home</Link>
-    );
-}
